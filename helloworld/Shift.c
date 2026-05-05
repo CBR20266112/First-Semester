92fd0940235f847e00248b2aacfe272b0b78fc34
CBR20266112 <test@example.com>
2026-05-05 15:32:31 +0900

    Add Shift.c and ASCII.c to helloworld folder

---
+++ helloworld/Shift.c
@@ -0,0 +1,10 @@
+// SHIFT OPERATOR test.
+#include <stdio.h>
+int main() {
+
+    int e = 2;
+    printf("e = %d\n", e);
+    printf("e+e = %d\t", e << 1);
+    printf("e/2 = %d\t", e >> 1);
+    printf("e^e = %d\t", e << 2);
+}
