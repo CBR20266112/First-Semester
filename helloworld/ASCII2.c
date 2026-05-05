e312310daff1d8603a76095aabd064f28efc8854
CBR20266112 <20266112@s.scnu.ac.kr>
2026-05-05 15:31:28 +0900

    Add files via upload

---
+++ helloworld/ASCII2.c
@@ -0,0 +1,10 @@
+// ASCII CODE test.2
+#include <stdio.h>
+int main() {
+
+    char CHARa = 'a';
+
+    printf("10진수: %d\t", CHARa);
+    printf("16진수: %x\n", CHARa);
+    printf("아스키코드 a + 5 = %d\t", CHARa + 5);
+}
