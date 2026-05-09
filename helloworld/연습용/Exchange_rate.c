a9d7254a30e053ebecc7aa3f50b54688b9f13da9
CBR20266112 <test@example.com>
2026-05-09 21:09:21 +0900

    커밋을하는데왜메시지가필요한가요?

--- helloworld/연습용/Exchange_rate.c
+++ helloworld/연습용/Exchange_rate.c
@@ -3,16 +3,16 @@
     
 int main(void) {
     double rate; // 원/달러 환율
-    double krw; // 원
     double usd; // 달러
+    int krw; // 원화
     
     printf("환율을 입력하시요: "); 
     scanf("%lf", &rate); 
     printf("원화 얼마요: ");
-    scanf("%lf", &krw);
+    scanf("%d", &krw);
 
     usd = krw / rate;
-    printf("원화 %lf이면 %lf딸라요\n", krw, usd);
+    printf("원화 %d이면 %lf딸라요.\n", krw, usd);
 
     return 0;
 }
