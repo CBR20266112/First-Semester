c23c41591c2967c032ed943b28f4fb575fbd35ac
CBR20266112 <test@example.com>
2026-05-10 04:24:29 +0900

    일단 만들라니 해보긴 하는데

---
+++ helloworld/연습용/다항싞.c
@@ -0,0 +1,12 @@
+// 다항식이요? 저는 난산증이 있어요 히이읶!!
+#include <stdio.h>
+int main(void) {
+    int x; int y;
+    printf("3x^2 + 7x + 11\n 실숫값을 직접 입력하시요.: ");
+    scanf("%d", &x);
+    y = 3x^2 + 7x + 11\n;
+
+    printf("다항식ㅇ르 대신 풀어주셔서 감사합니다,,: %d", y);
+
+    return 0;
+}
