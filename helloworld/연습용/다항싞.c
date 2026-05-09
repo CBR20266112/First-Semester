15b8307d4a67d1128caff0df034554a9aa38da0d
CBR20266112 <test@example.com>
2026-05-10 04:27:57 +0900

    제곱표현은 그냥 곱하기 직접 써줘야 한다고

--- helloworld/연습용/다항싞.c
+++ helloworld/연습용/다항싞.c
@@ -4,7 +4,7 @@ int main(void) {
     int x; int y;
     printf("3x^2 + 7x + 11\n 실숫값을 직접 입력하시요.: ");
     scanf("%d", &x);
-    y = 3x^2 + 7x + 11\n;
+    y = 3*x*x + 7*x + 11;
 
     printf("다항식ㅇ르 대신 풀어주셔서 감사합니다,,: %d", y);
 
