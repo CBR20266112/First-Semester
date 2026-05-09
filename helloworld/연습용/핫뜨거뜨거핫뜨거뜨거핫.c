30a67b4c3539fc3db0c30cc990eb799a6f7e391d
CBR20266112 <test@example.com>
2026-05-10 04:14:47 +0900

    왜안되는지 검색하기전에 우선 커밋해놓고

---
+++ helloworld/연습용/핫뜨거뜨거핫뜨거뜨거핫.c
@@ -0,0 +1,12 @@
+// 섭씨화씨
+#include <stdio.h>
+int main (void) {
+    int c; float f;
+    printf("화씨값을먼저입력해봐: ");
+    scanf("%f", &f);
+    c = (5/9) * (f-32);
+
+    printf("이소라목소리정말옥구슬이시다%d", c);
+
+    return 0;
+}
