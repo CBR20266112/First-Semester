9edbcd4fbe8e54a9135a1888825689cd72a63bbe
CBR20266112 <test@example.com>
2026-05-09 16:18:45 +0900

    commit

---
+++ helloworld/intfloatchar.c
@@ -0,0 +1,14 @@
+// INT FLOAT CHAR test.
+#include <stdio.h>
+int main() {
+    int i = 44.444444;
+    float f = 44.444444;
+    char c = '44.444444';
+
+
+    printf("int:%d\n",i);
+    printf("float:%f\n",f);
+    printf("char:%c\n",c);
+
+    return 0;
+}
