3bbf3e5d9cfcb1ff4d9b5dfed50b686bfd570f96
CBR20266112 <test@example.com>
2026-05-09 21:57:47 +0900

    올린다

---
+++ helloworld/연습용/Square.c
@@ -0,0 +1,14 @@
+// 사각형의 둘레와 면적
+#include <stdio.h> 
+int main(void) {
+
+    int w, h, area, perimeter;
+    
+    printf("가로와 세로 입력해: ");
+    scanf("%d %d", &w, &h);
+
+    area = w*h; perimeter = 2*(w+h);
+    printf("넓데데: %d, 뚤레뚤레: %d", &area, &perimeter);
+
+    return 0;
+}
