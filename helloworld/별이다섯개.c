06c0ae7f27816f8fffd17df8ad64c433fe38f1fc
CBR20266112 <test@example.com>
2026-05-09 16:23:44 +0900

    커밋한다고

---
+++ helloworld/별이다섯개.c
@@ -0,0 +1,12 @@
+//교수님너무어려워요학습부진아도조금은돌아봐주세요
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    for(i=1; i<=18; i++) {
+        for(int u=0; u<=i; u++) {
+            printf("*"); }
+        printf("\n"); }
+        return 0;
+    
+}
