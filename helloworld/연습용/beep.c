749b2a4bf24877bed62d285b7e9f807c72052705
CBR20266112 <test@example.com>
2026-05-10 23:22:12 +0900

    .

--- helloworld/연습용/beep.c
+++ helloworld/연습용/beep.c
@@ -1,4 +1,10 @@
 // 제어 문자 - 경고음 (교과서 발췌)
+#include <stdio.h>
+int main(void) {
 
-char beep = 7;
-printf("%c", beep);
+    char beep = 7;
+    printf("%c", beep);
+    //실행 환경상 경고음 출력은 안 되는 것으로 추정
+    char beeep = '\a';
+    printf("%c", beeep);
+}
---
+++ helloworld/연습용/escape.c
@@ -0,0 +1,15 @@
+// 특수 문자열 예제 (교과서 발췌)
+#include <stdio.h>
+int main(void) 
+{
+    int id, pass;
+
+    printf("아이디와 패스워드 숫자 4개로 쳐:\n");
+    printf("ID:____\b\b\b\b");    //화면의 커서를 뒤로 움직??
+    scanf("%d", &id);
+    printf("PASSWORD:____\b\b\b\b");    //이는 제어 문자라고?
+    scanf("%d", &pass);
+    printf("입력된 아이디는 \"%d\"이고 패스워드는 \"%d\"입니다?\n", id, pass);
+
+    return 0;
+}
