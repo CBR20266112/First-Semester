56393dab8a9a8b4b6ac5d07d57dd04c914ac84e7
CBR20266112 <test@example.com>
2026-05-10 20:39:20 +0900

    난산증이있다고여

--- helloworld/연습용/2complement.c
+++ helloworld/연습용/2complement.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    int x = 3;
-    int y = -3;
+    int x = 3;    //16진수 3도 결국 3이지
+    int y = -3;    //근데 이건 음수니까 2의 보수로?
     printf("x = %08X\n", x);    //%08X는 8자리16진수
     printf("y = %08X\n", y);    //대문자X는 대문자출력
     printf("x+y = %08X\n", x+y);    //앞에0은 출력하라고.
