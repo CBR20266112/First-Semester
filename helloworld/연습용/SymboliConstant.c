5c1adf8d22739ef39331c5f6fc91a235bcbf94cd
CBR20266112 <test@example.com>
2026-05-10 20:20:13 +0900

    일단 작성

---
+++ helloworld/연습용/SymboliConstant.c
@@ -0,0 +1,18 @@
+// 기호 상수 선언 2가지 방법 (교과서 발췌)
+#include<stdio.h>
+#define TAX_RATE 0.2    //#define을 이용한 기호상수 정의
+
+int main(void);
+{
+    const int MONTHS_ 12;    //const를 이용한 기호상수 정의
+    int m_salary, y_salary;    //얘는 변수 선언
+
+    printf("월급을 입력하십시ㅛ.: ");
+    scanf("%d", &m_salary);
+
+    y_salary = MONTHS * m_salary;
+    printf("연봉은 %d입니다.\n", y_salary);
+    printf("세금은 %d입니다. 도둑놈들아", y_salary*TAX_RATE);
+
+    return 0;
+}
--- helloworld/연습용/constant.c
+++ helloworld/연습용/constant.c
@@ -1,12 +1,16 @@
-// 상수리나무...
+// 상수리나무... 자료형 리터럴??
 #include <stdio.h>
 int main (void) {
 
     int x = 10; int y = 010; int z = 0x10;
+    char X = 010;    //자료형보다 리터럴이 우선?
+    int a = 30; int b = 030; int c = 0x30;
 
     printf("x = %d\n", x);
     printf("y = %d\n", y);
     printf("z = %d\n", z);
+    printf("X = %d\n", X);
+    printf("a = %d, b= %d, c = %d\t", a, b, c);
 
     return 0;
 
