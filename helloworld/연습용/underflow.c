e3ab228fa90bba9d93d213fece79bcfb0a62c6cd
CBR20266112 <test@example.com>
2026-05-10 21:08:47 +0900

    부동소수점 너무 어렵다

--- helloworld/연습용/underflow.c
+++ helloworld/연습용/underflow.c
@@ -5,11 +5,14 @@ int main(void)
 {
     float x = 1.23456e-38;
     float y = 1.23456e-40;
-    float z = 1.23456e-46l    //언더플로우????
-
+    float z = 1.23456e-46;   //언더플로우????
+    //e-38까진 괜찮은데 점점 부정확해지고 종래에는 0까지.
     printf("x = %e\n", x);
     printf("y = %e\n", y);
     printf("z = %d\n", z);
 
+    float value = 0.1;    //부동소수점은 부정확할 수 있대.
+    printf("%.20f\n", value);    //%.20f는 소수점 20자리 출력
+    //이진법에서는 아무리 노력해도 0.1을 정확히 표현할 수 없대.
     return 0;
 }
