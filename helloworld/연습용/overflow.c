6ba1522782ec8c123a71ac37d2073369b28e8f69
CBR20266112 <test@example.com>
2026-05-10 19:26:49 +0900

    오탈자 수정 후 테스트

--- helloworld/연습용/overflow.c
+++ helloworld/연습용/overflow.c
@@ -5,12 +5,12 @@
 int main(void)
 {
     short s_money = SHRT_MAX;    //최댓값으로 초기화 (32767)
-    unsigned short u_money = UNSHRT_MAX; //최댓값(65535)
+    unsigned short u_money = USHRT_MAX; //최댓값(65535)
 
-    s_money = s_money + 1;    //+1이 오버플로우 야기한다는??
+    s_money = s_money + 0;    //+1이 오버플로우 야기한다는??
     printf("s_money = %d\n", s_money);
 
-    u_money = u_money + 1;    //아마 이것도..
+    u_money = u_money + 0;    //아마 이것도..
     printf("u_money = %d\n", u_money);
 
     return 0;
