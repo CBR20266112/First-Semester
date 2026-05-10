d50294d72c749e0397264b053dcc0e48165f8f4d
CBR20266112 <test@example.com>
2026-05-10 23:28:21 +0900

    .

---
+++ helloworld/연습용/sum_error.c
@@ -0,0 +1,13 @@
+// 변수를 사용할 때 가장 주의할 점은 초기화되지 않은 값을 사용하는 것이다. (라고 교과서가 말했습니다.)
+#include <stdio.h>
+int main(void) {
+    int x, y, z, sum;
+    printf("세개의정수 입력하시요.: ");
+    scanf("%d %d %d", &x, &y, &z);
+    sum += x;
+    sum += y;
+    sum += z;
+    printf("3개 정수의 합이요.: %d", sum);
+
+    return 0;
+}
