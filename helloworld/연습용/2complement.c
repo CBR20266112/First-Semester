2a68738c9e0e37812ab19755d3bc441bbfbe7c4d
CBR20266112 <test@example.com>
2026-05-10 20:33:36 +0900

    작성중

---
+++ helloworld/연습용/2complement.c
@@ -0,0 +1,13 @@
+// 2의 보수 프로그람 (교과서 발췌)
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 3;
+    int y = -3;
+    printf("x = %08X\n", x);    //%08X는 8자리16진수
+    printf("y = %08X\n", y);    //대문자X는 대문자출력
+    printf("x+y = %08X\n", x+y);    //앞에0은 출력하라고.
+
+    return 0;
+}
