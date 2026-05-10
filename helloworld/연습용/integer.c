45b8468a630304153fbe87d3ce9e93072d86b09b
CBR20266112 <test@example.com>
2026-05-10 19:11:20 +0900

    교재 따라해보기

---
+++ helloworld/연습용/integer.c
@@ -0,0 +1,16 @@
+// 정수 자료형을 사용하는 프로그램 (교과서 발췌)
+#include <stdio.h>
+
+int main(void) 
+{
+    short year = 0;   //short형 변수 선언
+    int sale = 0;    //int형 변수 선언
+    long total_sale = 0;    //long형 변수 선언
+    long long large_value;    //64비트 정수형
+
+    year = 10;    //약 3만 2천을 넘지 않도록 주의
+    sale = 200000000;    //약 21억을 넘지 않도록 주의
+    total_sale = year*sale;    //약 21억..넘지말라고
+
+    printf("total_sale = %d\n", total_sale);
+}
