7d276cb5939fd1542aeb0d53f585ff6ad9c97f3f
CBR20266112 <test@example.com>
2026-05-11 00:59:35 +0900

    컴파일러는 띄어쓰기를 인식하지 못하네!

--- helloworld/연습용/8-10-16진수.c
+++ helloworld/연습용/8-10-16진수.c
@@ -4,6 +4,7 @@ int main(void) {
 
    int h;
    printf("16진수 정수 입력해봐.: "); scanf("%x", &h);
-   printf("8진수:%o, 10진수:%d, 16진수:%x", h, h, h);
+   printf("8진수:%o, 10진수:%d, 16진수:%#x", h, h, h);
+   //16진수를 0을 붙여 출력받으려면 %#x여야 한다고.
    return 0; 
 }
---
+++ helloworld/연습용/xy_trade.c
@@ -0,0 +1,14 @@
+// int형 변수 x랑 y값 서로 교환??
+#include <stdio.h>
+int main(void)
+{
+    int x = 10; int y = 20; int z;
+    printf("x = %d, y = %d\n" x, y);
+    z = x;
+    x = y;
+    y = z;
+    printf("짜잔\n");
+    printf("x = %d, y = %d", x, y);
+
+    return 0;
+}
