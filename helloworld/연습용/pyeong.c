d60e0297ed5c2e861c6a9a5c2696c5842549e77e
CBR20266112 <test@example.com>
2026-05-11 01:39:22 +0900

    하너무어렵다고

---
+++ helloworld/연습용/pyeong.c
@@ -0,0 +1,11 @@
+// 평을 제곱미터로 환산하는 프로그램을 작성하래.
+#include <stdio.h>
+int main(void)
+{
+    float pyeong; double m^2
+    printf("몇평인데?: ") scanf("%f", &pyeong);
+    m^2 = 3.3058 * pyeong
+    printf("그러면 대충 %lf평방미터래.", m^2);
+
+    return 0;
+}
