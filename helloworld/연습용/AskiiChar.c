4c0a993832e1424d42f1916528b3efe225763527
CBR20266112 <test@example.com>
2026-05-12 02:27:09 +0900

    뼈찜맛있다

--- helloworld/연습용/AskiiChar.c
+++ helloworld/연습용/AskiiChar.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 int main(void)
 {
-    char a;
+    char a = 'a'; int b;
     printf("아스키 코드값을 입력하시요.: ");
-    scanf("%d", &a);
-    printf("문자로 %c입니다.", a);
-    
+    scanf("%d", &b);
+    printf("문자로 %c입니다.\n\n", b);
+    //아스키코드에 더하기까지 하래.
+    printf("아스키 코드 a+1 = %d\t", a+1);
+    printf("아스키 코드 a+2 = %d\t", a+2);
+    printf("아스키 코드 a+3 = %d", a+3);
+
     return 0;
 }
