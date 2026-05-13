a16b7c8a0dafff4ad708f16f93a27d7aa407269c
CBR20266112 <test@example.com>
2026-05-13 22:57:41 +0900

    비트연산자 다 잊어먹음

---
+++ helloworld/연습용2/bit_oper.c
@@ -0,0 +1,8 @@
+// 비트 연산자
+#include <stdio.h>
+int main(void) {
+    printf("AND: %08X\n", 0X9 & 0XA);
+    printf("OR: %08x\n", 0X9 | 0XA);
+    printf("NOT: %08x\n", 0X9 ! 0XA);
+    printf("XOR: %08x\n", 0X9 )
+}
