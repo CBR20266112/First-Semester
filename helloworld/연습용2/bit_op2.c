5121b24ff2805e8bb083601cef034c9f0ea59f54
CBR20266112 <test@example.com>
2026-05-13 23:10:08 +0900

    왜또오류

---
+++ helloworld/연습용2/bit_op2.c
@@ -0,0 +1,10 @@
+// NOT 연산자를 이용해 마이너스 출력하기인 듯. (교과서 발췌)
+#include <stdio.h>
+int main(void) {
+    int a = 32;
+    a = ~a;    //일단 비트 반전하는 듯, NOT 연산자를 이용해서.
+    a = a + 0x01;    //16진수 기준 1 더하기인가?
+    printf('a = %d\n', a);
+
+    reurn 0;
+}
--- helloworld/연습용2/bit_oper.c
+++ helloworld/연습용2/bit_oper.c
@@ -1,8 +1,12 @@
 // 비트 연산자
 #include <stdio.h>
 int main(void) {
-    printf("AND: %08X\n", 0X9 & 0XA);
+    printf("AND: %08X\n", 0X9 & 0XA);    //9 AND 10인거지, 16진수 기준으로.
     printf("OR: %08x\n", 0X9 | 0XA);
-    printf("NOT: %08x\n", 0X9 ! 0XA);
-    printf("XOR: %08x\n", 0X9 )
+    printf("NOT: %08x\n", ~0X9);
+    printf("XOR: %08x\n", 0X9 ^ 0XA);
+    printf("<<: %08x\n", 0X4 << 1);
+    printf(">>: %08x", 0X4 >> 1);
+
+    return 0;
 }
