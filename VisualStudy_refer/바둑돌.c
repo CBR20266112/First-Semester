b90230bc8e6c4b453a27fd8af7159154c2183b60
CBR20266112 <test@example.com>
2026-05-23 07:25:12 +0900

    어려워..

--- VisualStudy_refer/바둑돌.c
+++ VisualStudy_refer/바둑돌.c
@@ -5,8 +5,8 @@ int main(void) {
     printf("바둑돌 세로로 몇 개?"); int n, b, i;
     scanf("%d", &n); b = 0;
 
-    for(i = 0; i >= n; i++) {
-        b = 0 + n;
+    for(i = 1; i <= n; i++) {
+        b += i;    //=b=b+i; 누적해서 더하기 위해 복합대입연산자 사용해야 함.
     }
     printf("바둑돌 %d단계면 총 %d개", n, b);
     
